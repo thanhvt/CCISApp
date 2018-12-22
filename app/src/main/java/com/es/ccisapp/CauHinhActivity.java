7930415dc4e25da137899096b03777c391b3a885
ThanhVT <vutienthanh0210@gmail.com>
2018-12-22 23:57:18 +0700

    Settings Screen

--- app/src/main/java/com/es/ccisapp/CauHinhActivity.java
+++ app/src/main/java/com/es/ccisapp/CauHinhActivity.java
@@ -2,9 +2,14 @@ package com.es.ccisapp;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.preference.EditTextPreference;
 import android.support.v7.preference.PreferenceFragmentCompat;
 import android.support.v7.preference.PreferenceManager;
+import android.util.Log;
+import android.view.MenuItem;
 
 public class CauHinhActivity extends AppCompatActivity {
 
@@ -18,11 +23,27 @@ public class CauHinhActivity extends AppCompatActivity {
                 .replace(R.id.content, new SettingsFragment())
                 .commit();
 
+        ActionBar actionBar = this.getSupportActionBar();
+
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
         SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
-//        String strUserName = sharedPrefs.getString("username", "NA");
+        String IP_SERVICE = sharedPrefs.getString("IP_SERVICE", "0");
+        Log.e("CauHinhActivity", IP_SERVICE);
 //        boolean bAppUpdates = sharedPrefs.getBoolean("applicationUpdates",false);
 //        String downloadType = sharedPrefs.getString("downloadType","1");
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            NavUtils.navigateUpFromSameTask(this);
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public static class SettingsFragment extends PreferenceFragmentCompat {
@@ -30,6 +51,10 @@ public class CauHinhActivity extends AppCompatActivity {
         public void onCreatePreferences(Bundle bundle, String s) {
             // Load the Preferences from the XML file
             addPreferencesFromResource(R.xml.pref_general);
+            SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(getContext());
+            String IP_SERVICE = sharedPrefs.getString("IP_SERVICE", "0");
+            EditTextPreference etp = (EditTextPreference) findPreference("IP_SERVICE");
+            etp.setSummary(IP_SERVICE);
         }
     }
 }
