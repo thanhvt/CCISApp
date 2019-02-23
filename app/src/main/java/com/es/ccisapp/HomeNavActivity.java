0e8f39ebbf6fbe16ff7f89943702b302ee07d70d
ThanhVT <vutienthanh0210@gmail.com>
2019-02-23 12:42:20 +0700

    Thu tiền offline

--- app/src/main/java/com/es/ccisapp/HomeNavActivity.java
+++ app/src/main/java/com/es/ccisapp/HomeNavActivity.java
@@ -85,7 +85,6 @@ public class HomeNavActivity extends AppCompatActivity { // implements Navigatio
 //                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
 //        drawer.addDrawerListener(toggle);
 //        toggle.syncState();
-
 //        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
 //        navigationView.setNavigationItemSelectedListener(this);
 
